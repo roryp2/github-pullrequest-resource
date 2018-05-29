81d6a1ee9e69c8f948bab4fcd5bcdbf1ad4b78e1
Rory Price-Coggins <roryprice21@gmail.com>
2018-05-29 08:34:57 +0100

    fix

--- assets/lib/filters/all.rb
+++ assets/lib/filters/all.rb
@@ -20,7 +20,7 @@ module Filters
     attr_reader :input
 
     def pull_options
-      options = { state: 'open', sort: 'updated', direction: 'asc'} }
+      options = { state: 'open', sort: 'updated', direction: 'asc' }
       options[:base] = input.source.base if input.source.base
       options
     end
